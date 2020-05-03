6280756a824c10d2ec30004ef23f9f8db4a65fcb
Mark Grant <m.grant.prg@gmail.com>
2020-05-03 10:12:29 +0100

    Comment to emphasise kbx requirement for gpgv
    Signed-off-by: Mark Grant <m.grant.prg@gmail.com>

--- hook.d/D40-gpgv-check-dsc.sh
+++ hook.d/D40-gpgv-check-dsc.sh
@@ -3,7 +3,7 @@
 #########################################################################
 #									#
 # Script ID: D40-gpgv-check-dsc.sh					#
-# Author: Copyright (C) 2018  Mark Grant				#
+# Author: Copyright (C) 2018, 2020  Mark Grant				#
 #									#
 # Released under the GPLv3 only.					#
 # SPDX-License-Identifier: GPL-3.0					#
@@ -13,7 +13,7 @@
 #									#
 # The gpgv run by pbuilder -always- fails. Whilst that pbuilder check	#
 # still runs and fails, it is now followed by this hook script which	#
-# works.								#
+# works. gpgv requires the keybox 'trustedkeys.kbx'.			#
 #									#
 # With a script name starting with 'D' this is run during chroot setup,	#
 # before dependencies are evaluated and before package building has	#
@@ -28,6 +28,9 @@
 # Date		Author	Version	Description				#
 #									#
 # 26/02/2018	MG	1.0.1	Created.				#
+# 02/05/2020	MG	1.0.2	Update file comment above to include	#
+#				note on the requirement for the		#
+#				trustedkeys.kbx keybox.			#
 #									#
 #########################################################################
 
